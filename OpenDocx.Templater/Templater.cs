1278a34b6a0ab74eeb3484a19d3bc846a195b42a
Lowell Stewart <lowell@opendocx.com>
2022-06-23 21:28:16 -0600

    Tweak new workspace/project settings

--- OpenDocx.Templater/Templater.cs
+++ OpenDocx.Templater/Templater.cs
@@ -1,9 +1,9 @@
 /***************************************************************************
 
-Copyright (c) Lowell Stewart 2018-2019.
+Copyright (c) Lowell Stewart 2018-2022.
 Licensed under the Mozilla Public License. See LICENSE file in the project root for full license information.
 
-Published at https://github.com/opendocx/opendocx
+Published at https://github.com/opendocx/opendocx-net
 Developer: Lowell Stewart
 Email: lowell@opendocx.com
 
